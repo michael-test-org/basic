155367e74787a1faa09c63fbd606ca1d0e400900
Michael Heap <m@michaelheap.com>
2018-05-21 12:20:13 +0300

    Do Work

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Thanks for watching
                 </div>
 
                 <div class="links">
